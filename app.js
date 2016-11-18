c38f164ecc8fcf93b06dfb1f384e78197601ddc4
felipemmasan <felipe.m.masan@gmail.com>
2016-11-18 13:20:14 -0200

    Criação do bot

---
+++ app.js
@@ -0,0 +1,16 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+var server = restify.createServer();
+server.listen(process.env.PORT || 3000, function()
+{
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+var connector = new builder.ChatConnector({appId: 'appID', appPassword: 'appPassword'});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+bot.dialog('/', function(session){
+    session.send("Olá")
+});
