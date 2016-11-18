2638c9c8857e2a1771d531118c0b509b6fd1d94b
felipemmasan <felipe.m.masan@gmail.com>
2016-11-18 13:29:56 -0200

    Incluídos as keys do App

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ server.listen(process.env.PORT || 3000, function()
     console.log('%s listening to %s', server.name, server.url);
 });
 
-var connector = new builder.ChatConnector({appId: 'appID', appPassword: 'appPassword'});
+var connector = new builder.ChatConnector({appId: 'c0d8fc91-5c91-4f44-ab4b-0ab1060b658e', appPassword: '9BxkRdSRF5N1uD8bkcd0PFh'});
 var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
+server.post('/API/Messages', connector.listen());
 
 bot.dialog('/', function(session){
     session.send("Olá")
