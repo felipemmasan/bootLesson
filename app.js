90fd9447e4534788ae0a11b0807317024f9e8cf2
Felipe Santos <felipe.m.masan@gmail.com>
2018-05-10 13:35:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ server.listen(process.env.PORT || 3000, function()
     console.log('%s listening to %s', server.name, server.url);
 });
 
-var connector = new builder.ChatConnector({appId: 'c0d8fc91-5c91-4f44-ab4b-0ab1060b658e', appPassword: '9BxkRdSRF5N1uD8bkcd0PFh'});
+var connector = new builder.ChatConnector({appId: '######', appPassword: '#########'});
 var bot = new builder.UniversalBot(connector);
 
 //Inicializa o serviço
